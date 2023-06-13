b5f4822b2660e69e0e1b96142e572a7df4e6ff1c
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2023-06-13 16:14:41 +0200

    wip things

--- ortegapp-web/src/app/pages/producto/home.component.ts
+++ ortegapp-web/src/app/pages/producto/home.component.ts
@@ -1,23 +1,47 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router, NavigationEnd } from '@angular/router';
 import { untilDestroyed } from '@ngneat/until-destroy';
 import { delay, filter } from 'rxjs/operators';
+import { Producto } from 'src/app/interface/producto.interface';
+import { ProductoService } from 'src/app/service/producto-service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: './producto.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
+  productoList:  Producto[] = [];
+  numPages = 0;
 
-  constructor(private observer: BreakpointObserver, private router: Router) {}
+  public displayedColumns: string[] = ['ID', 'Nombre', 'Tipo', 'Precio'];
+  public columnsToDisplay: string[] = [...this.displayedColumns, 'actions'];
+  public dataSource!: MatTableDataSource<Producto>;
+
+
+  constructor(private observer: BreakpointObserver, private router: Router, private productoService : ProductoService) {}
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.getProducto(0)
+    
+  }
+
+
+  getProducto(page : number){
+    this.productoService.getProductos(page).subscribe(resp =>{
+      this.productoList = resp.content
+      this.numPages = Math.ceil(resp.totalPages);
+      this.dataSource.data = resp.content
+      console.log(this.productoList)
+      console.log("Jhony walker")
+      console.log(resp.content)
+    })
+
   }
 
   ngAfterViewInit() {
---
+++ ortegapp-web/src/app/service/producto-service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductoResponse } from '../interface/producto.interface';
+import { environment } from 'src/environments/environment';
+import { TokenStorageService } from './token-storage-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+  
+
+  constructor(private http: HttpClient, private tokenService : TokenStorageService) { }
+
+  getProductos(page : number) : Observable<ProductoResponse>{
+    console.log("blue label")
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenService.getToken()}`,
+    
+
+    });
+    console.log(this.tokenService.getToken())
+    return this.http.get<ProductoResponse>(`${environment.urlBase}/producto?page=${page}` );
+
+  }
+}
