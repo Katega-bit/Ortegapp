59d58eb032b9114b96b8256ed408e3eb0774c930
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2023-06-14 12:27:26 +0200

    show products an user done

--- ortegapp-web/src/app/pages/producto/home.component.ts
+++ ortegapp-web/src/app/pages/producto/home.component.ts
@@ -1,6 +1,8 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSidenav } from '@angular/material/sidenav';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router, NavigationEnd } from '@angular/router';
 import { untilDestroyed } from '@ngneat/until-destroy';
@@ -13,7 +15,7 @@ import { ProductoService } from 'src/app/service/producto-service';
   templateUrl: './producto.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+/* export class HomeComponent implements OnInit {
 
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
@@ -69,4 +71,64 @@ export class HomeComponent implements OnInit {
         }
       });
   }
+} */
+
+export class HomeComponent implements OnInit{
+  // TODO COLUMNS NAME
+  displayedColumns: string[] = ['Id', 'Nombre', 'Tipo', 'Precio'];
+  
+  // TODO DATA RESPONSE
+  dataSource!: MatTableDataSource<Producto>;
+  
+  // NEVER FALSE
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+
+  products: Producto[] = [];
+  index = 0;
+
+
+  totalPages: number = 0;
+  totalElements: number = 0;
+
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  
+  constructor(private productoService : ProductoService){
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource<Producto>();
+  }
+  
+  ngOnInit(): void {
+    this.getAllProductByPageIndex(this.index);
+  }
+
+
+  getAllProductByPageIndex(page : number){
+    this.productoService.getProductos(page).subscribe((res => {
+      this.products = res.content;
+      this.dataSource.data = this.products;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.totalElements = res.totalElements;
+      this.totalPages = res.totalPages;
+
+    }))
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 }
--- ortegapp-web/src/app/pages/profile/profile.component.ts
+++ ortegapp-web/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Producto } from 'src/app/interface/producto.interface';
+import { User } from 'src/app/interface/user.interface';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +13,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['Id', 'Username', 'FullName','Email', 'Actions'];
+  
+  // TODO DATA RESPONSE
+  dataSource!: MatTableDataSource<User>;
+  
+  // NEVER FALSE
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit(): void {
+
+
+  users: User[] = [];
+  index = 0;
+
+
+  totalPages: number = 0;
+  totalElements: number = 0;
+
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
+  
+  constructor(private userService : UserService){
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource<User>();
+  }
+  
+  ngOnInit(): void {
+    this.getAllUsersByPageIndex(this.index);
+  }
+
+
+  getAllUsersByPageIndex(page : number){
+    this.userService.getUsers(page).subscribe((res => {
+      this.users = res.content;
+      this.dataSource.data = this.users;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.totalElements = res.totalElements;
+      this.totalPages = res.totalPages;
+
+    }))
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 
 }
